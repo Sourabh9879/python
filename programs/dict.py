f8660788b0696cb5cddf943fad35c6425e0ccb9f
Sourabh9879 <kumarsourabh970@gmail.com>
2025-08-21 18:28:16 +0530

    dictionary and set

---
+++ programs/dict.py
@@ -0,0 +1,12 @@
+dict = {
+
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+
+print(dict)
+dict["color"] = "black"
+print(dict)
+dict.pop("color")
+print(dict)
---
+++ programs/sets.py
@@ -0,0 +1,8 @@
+myset = {"abc","xyz","pqr"}
+for x in myset:
+    print(x)
+
+myset.add("hello")
+print(myset)
+myset.remove("hello")
+print(myset)
