362cf4dc214015e075eece90e747884bf6e80148
Sourabh9879 <kumarsourabh970@gmail.com>
2025-08-22 19:35:38 +0530

    class and objects

---
+++ programs/initcons.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def myFunc(self):
+        print("hello my name is " + self.name)
+
+p1 = Person("Rahul",20)
+p1.myFunc()
+# print(p1.name)
+# print(p1.age)
