330187f5ab2bd7d1fab2c47af8f3198659767d73
Sourabh9879 <kumarsourabh970@gmail.com>
2025-08-22 19:04:59 +0530

    functions

---
+++ programs/function.py
@@ -0,0 +1,9 @@
+def my_func(a):
+    cars = ["mustang","Bmw","Dzire"]
+    for x in cars:
+        if x == "Bmw":
+            print("i like bmw")
+        print(x)
+    print("hello",  a )
+
+my_func("sourabh")
